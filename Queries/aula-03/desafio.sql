2e45a0c46332753e7219378d919451f881afcf79
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-06-20 11:41:16 -0300

    Criando a tabela de vendedores

---
+++ Queries/aula-03/desafio.sql
@@ -0,0 +1,11 @@
+/*Nome da tabela deve ser TABELA_DE_VENDEDORES
+Vendedor tem o número interno da matrícula, onde será armazenado no campo MATRICULA, que deve ser um string de 5 posições.
+O nome do vendedor deverá ser armazenado no campo NOME, e deve ser um string de 100 posições.
+Criar o campo PERCENTUAL_COMISSAO que representa quantos % de comissão o vendedor ganha sobre cada venda.*/
+
+CREATE TABLE TABELA_DE_VENDEDORES
+(
+MATRICULA VARCHAR(5),
+NOME VARCHAR (100),
+PERCENTUAL_COMISSAO FLOAT
+)
